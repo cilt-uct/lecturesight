06552844497738d4f8ec1a44331c3188a92e4f1d
Stephen Marquard <stephen.marquard@uct.ac.za>
2016-04-08 11:17:01 +0200

    Preview RPi overview cam using gstreamer pipeline

---
+++ runtime/rpi-preview.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# set correct base dir for production and daemon operation
+BASE_DIR="/opt/ls"
+cd $BASE_DIR
+
+# Check serial link type and set up socat if required
+RTP=`grep "^cv.lecturesight.framesource.input.mrl=rtph264://" $BASE_DIR/conf/lecturesight.properties`
+
+# Match RPi hostname without the port
+if [[ "$RTP" =~ rtph264://([a-z0-9.-]+) ]]; then
+        RPI=${BASH_REMATCH[1]}
+        echo RPi overview camera: $RPI
+	gst-launch-1.0 tcpclientsrc host=$RPI port=8554 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false 
+fi
+
