e57d397bef41e05165475b627eb78cc606e2d02c
Stephen Marquard <stephen.marquard@uct.ac.za>
2017-12-16 20:11:30 +0200

    LS-225 Add spline interpolators

--- src/modules/lecturesight-utils/src/main/java/cv/lecturesight/util/geometry/CameraPositionModel.java
+++ src/modules/lecturesight-utils/src/main/java/cv/lecturesight/util/geometry/CameraPositionModel.java
@@ -17,6 +17,11 @@
  */
 package cv.lecturesight.util.geometry;
 
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+
+import java.util.Arrays;
+
 /**
  * Convert between normalized co-ordinates and camera pan/tilt co-ordinates
  */
@@ -43,6 +48,23 @@ public class CameraPositionModel {
   // target position in normalized coordinates
   private NormalizedPosition camera_posn = new NormalizedPosition(0.0f, 0.0f);
 
+  // Interpolation functions
+  private PolynomialSplineFunction normCameraX;
+  private PolynomialSplineFunction normCameraY;
+  private PolynomialSplineFunction cameraNormX;
+  private PolynomialSplineFunction cameraNormY;
+
+  // Ranges over which the interpolation functions are valid
+  private float minNormX = 1;
+  private float maxNormX = -1;
+  private float minNormY = 1;
+  private float maxNormY = -1;
+
+  private int minCameraX = Integer.MAX_VALUE;
+  private int maxCameraX = Integer.MIN_VALUE;
+  private int minCameraY = Integer.MAX_VALUE;
+  private int maxCameraY = Integer.MIN_VALUE;
+
   /**
    * Initialize camera position model using scene limits
    * @param pan_min
@@ -58,11 +80,73 @@ public class CameraPositionModel {
    * Update camera model from scene limits
    */
   public void update(int pan_min, int pan_max, int tilt_min, int tilt_max) {
-    this.pan_max = pan_max;
+
+    if (pan_min != this.pan_min || pan_max != this.pan_max || tilt_min != this.tilt_min || tilt_max != this.tilt_max) {
+      // Invalidate the spline functions if the limits have changed
+      normCameraX = null;
+      normCameraY = null;
+      cameraNormX = null;
+      cameraNormY = null;
+    }
+
     this.pan_min = pan_min;
-    this.tilt_max = tilt_max;
+    this.pan_max = pan_max;
     this.tilt_min = tilt_min;
-    return;
+    this.tilt_max = tilt_max;
+  }
+
+  /**
+   * Update the scene model from a set of matching presets.
+   * The point sceneMarker[n] on the overview image matches the camera position cameraPreset[n].
+   *
+   * @param sceneMarker  Array of scene marker co-ordinates
+   * @param cameraPreset Array of camera preset co-ordinates matching the scene markers
+   * @return true if the model has been updated based on the preset co-ordinates.
+   */
+  public boolean update(NormalizedPosition[] sceneMarker, Position[] cameraPreset) {
+
+    int points = sceneMarker.length;
+
+    if ((points < 3) || (points != cameraPreset.length)) {
+      return false;
+    }
+
+    // X Values
+    double[] xNorm = new double[points];
+    double[] xCamera = new double[points];
+
+    // Y values
+    double[] yNorm = new double[points];
+    double[] yCamera = new double[points];
+
+    for (int i = 0; i < points; i++) {
+      xNorm[i] = (double) sceneMarker[i].getX();
+      xCamera[i] = (int) cameraPreset[i].getX();
+      yNorm[i] = (double) sceneMarker[i].getY();
+      yCamera[i] = (int) cameraPreset[i].getY();
+    }
+
+    Arrays.sort(xNorm);
+    Arrays.sort(xCamera);
+    Arrays.sort(yNorm);
+    Arrays.sort(yCamera);
+
+    minNormX = (float) xNorm[0];
+    maxNormX = (float) xNorm[points-1];
+    minNormY = (float) yNorm[0];
+    maxNormY = (float) yNorm[points-1];
+
+    minCameraX = (int) xCamera[0];
+    maxCameraX = (int) xCamera[points-1];
+    minCameraY = (int) yCamera[0];
+    maxCameraY = (int) yCamera[points-1];
+
+    cameraNormX = new SplineInterpolator().interpolate(xCamera, xNorm);
+    normCameraX = new SplineInterpolator().interpolate(xNorm, xCamera);
+    normCameraY = new SplineInterpolator().interpolate(yNorm, yCamera);
+    cameraNormY = new SplineInterpolator().interpolate(yCamera, yNorm);
+
+    return true;
   }
 
   /**
@@ -77,8 +161,26 @@ public class CameraPositionModel {
     float x = pos.getX();
     float y = pos.getY();
 
-    out.setX((x - pan_min) / (pan_max - pan_min) * 2 - 1);
-    out.setY((y - tilt_min) / (tilt_max - tilt_min) * 2 - 1);
+    if ((cameraNormX != null) && (cameraNormY != null)) {
+
+      if ((pos.getX() >= minCameraX) && (pos.getX() <= maxCameraX)) {
+        // Spline interpolation
+        out.setX((float) cameraNormX.value((double) pos.getX()));
+      } else {
+        // TODO Linear
+      }
+
+      if ((pos.getY() >= minCameraY) && (pos.getX() <= maxCameraY)) {
+        // Spline interpolation
+        out.setY((float) cameraNormY.value((double) pos.getY()));
+      } else {
+        // TODO Linear
+      }
+    } else {
+      // Linear mapping
+      out.setX((x - pan_min) / (pan_max - pan_min) * 2 - 1);
+      out.setY((y - tilt_min) / (tilt_max - tilt_min) * 2 - 1);
+    }
 
     return out;
   }
@@ -95,8 +197,25 @@ public class CameraPositionModel {
     float x = pos.getX();
     float y = pos.getY();
 
-    out.setX((int) ((x + 1) * (pan_max - pan_min) * 0.5 + pan_min));
-    out.setY((int) ((y+1) * (tilt_max - tilt_min) * 0.5 + tilt_min));
+    if ((normCameraX != null) && (normCameraY != null)) {
+      if ((pos.getX() >= minNormX) && (pos.getX() <= maxNormX)) {
+        // Spline interpolation
+        out.setX((int) normCameraX.value((double) pos.getX()));
+      } else {
+        // TODO Linear
+      }
+
+      if ((pos.getY() >= minNormY) && (pos.getY() <= maxNormY)) {
+        out.setY((int) normCameraY.value((double) pos.getY()));
+      } else {
+        // TODO Linear
+      }
+
+    } else {
+      // Linear mapping
+      out.setX((int) ((x + 1) * (pan_max - pan_min) * 0.5 + pan_min));
+      out.setY((int) ((y + 1) * (tilt_max - tilt_min) * 0.5 + tilt_min));
+    }
 
     return out;
   }
