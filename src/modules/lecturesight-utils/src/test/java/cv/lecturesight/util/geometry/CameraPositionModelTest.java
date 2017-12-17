95dd4e7e0671ab72d40daf7625f2bf100b662af5
Stephen Marquard <stephen.marquard@uct.ac.za>
2017-12-17 11:48:10 +0200

    LS-225 Add surefire test support

---
+++ src/modules/lecturesight-utils/src/test/java/cv/lecturesight/util/geometry/CameraPositionModelTest.java
@@ -0,0 +1,40 @@
+package cv.lecturesight.util.geometry;
+
+import org.junit.Assert;
+
+public class CameraPositionModelTest {
+
+  @org.junit.Before
+  public void setUp() throws Exception {
+  }
+
+  @org.junit.After
+  public void tearDown() throws Exception {
+  }
+
+  @org.junit.Test
+  public void update() {
+
+    Assert.assertTrue(true);
+  }
+
+  @org.junit.Test
+  public void update1() {
+  }
+
+  @org.junit.Test
+  public void findEdge() {
+  }
+
+  @org.junit.Test
+  public void extrapolate() {
+  }
+
+  @org.junit.Test
+  public void toNormalizedCoordinates() {
+  }
+
+  @org.junit.Test
+  public void toCameraCoordinates() {
+  }
+}
